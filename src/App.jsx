6fba7114bad7e2b8ea791ca056506335140e0d11
adilene espinosa herrera <adileneherrera193@gmail.com>
2023-02-27 11:44:27 -0600

    creando lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 function App() {
-
+  const name = "Adilene Espinosa Herrera"
+  return(
+      <div>
+          <h3>Lista de hobbies de {name}</h3>
+          <ul>
+              <li>ver series</li>
+              <li>Escuchar Música</li>
+              <li>Dormir</li>
+          </ul>
+      </div>
+  )
 }
 
 export default App
